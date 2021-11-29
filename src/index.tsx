59d85a54f3db9bafacaa34d0b3d736dc645e3f3b
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-29 19:45:59 +0545

    global styles and theme provider setup for styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+// Components
 import App from './App';
+
+// Theme
+import { theme } from './assets/styles/theme';
+
+// Global Styles
+import GlobalStyle from './assets/styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
