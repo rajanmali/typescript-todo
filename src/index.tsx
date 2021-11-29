95acfefd276b228223b922329391c19ae90f97df
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-29 20:21:20 +0545

    update tsconfig path file for new folders

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,18 @@ import { ThemeProvider } from 'styled-components';
 
 // Components
 import App from './App';
-import Header from '@components/layout/header';
 
 // Theme
 import { theme } from '@config/theme';
 
 // Global Styles
-import GlobalStyle from './assets/styles/global';
+import GlobalStyle from '@assets/styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
-      <Header />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
